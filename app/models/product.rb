0ca2d75c6320383bd8668a26ebda41861f35f93b
Juan Pablo <juanpablogast@gmail.com>
2021-06-08 20:12:38 -0400

    relaciones

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
 
   has_many :order_items
   has_many :orders, through: :order_items
+
+  has_many :variants
+  has_many :sizes, through: :variants
+
+  has_many :colors, through: :variants
+
 end
