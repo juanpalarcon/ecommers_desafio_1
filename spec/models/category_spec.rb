a2b47336721103546ff930b62f1f35e34814b6de
Juan Pablo <juanpablogast@gmail.com>
2021-06-08 22:12:53 -0400

    ordenando

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-    context 'when category does not have children ' do
+  context 'when category does not have children ' do
     let(:category_without_children) { create(:category) } 
     let(:category_1) { create(:category) }
     let(:category_2) { create(:category, category_id: category_1.id) }
@@ -10,12 +10,25 @@ RSpec.describe Category, type: :model do
       expect(category_without_children.name).to eq('description')
     end
 
-    it 'returns nochiledren' do 
+    it 'returns no chiledren' do 
       expect(category_without_children.children).to eq([])
     end
 
-    it 'returns parent correctly' do 
-      expect(category_2.parent).to eq(category_1)
+    context 'when category has childrens' do 
+
+      it 'returns parent correctly' do 
+        expect(category_2.parent).to eq(category_1)
+      end
+
+      it 'returns children correctly' do
+        expect(category_1.children).to eq([category_2])
+      end
+    end
+
+    context ' when category does not have parents' do
+      it 'return without parents' do
+        expect(category_1.parent).to eq(nil)
+      end
     end
 
   end
