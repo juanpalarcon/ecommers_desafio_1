c17a6930fcbd5421bbd388b012c1fad7fd95398c
Juan Pablo <juanpablogast@gmail.com>
2021-06-11 21:38:03 -0400

    visible con catalogo

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,15 @@ class Product < ApplicationRecord
   has_many :sizes, through: :variants
   has_many :colors, through: :variants
 
+# 
+  def visible_con_catalogo?
+    sel.variants.each do |variant| # buscamos cada stock del producto
+      if variant.stock > 0 # si es mayor a 0 
+        return true # el producto se muestra en catalogo
+      else # si no 
+        false # no se mostrara en el catalogo
+      end
+    end
+  end
+
 end
