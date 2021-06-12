3a0c6ec38c908dd2d66bc70a06df9c49e30a3033
Juan Pablo <juanpablogast@gmail.com>
2021-06-11 21:03:03 -0400

    metodo all_children y all_parents

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,20 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
-  has_many :children, class_name: 'Category'
+  has_many :children, class_name: 'Category', dependent: :destroy
   belongs_to :parent, class_name: 'Category', foreign_key: :category_id, optional: true
+
+
+  # scope :parents, -> {where(parent: nil)}
+
+  def all_children
+    self.children.flat_map do |child|
+      child.all_children << child
+    end
+  end
+
+
+  def self.all_parents
+    where(:category_id :nil)
+  end
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   context 'when category does not have children ' do
     let(:category_without_children) { create(:category) } 
+    
     let(:category_1) { create(:category) }
     let(:category_2) { create(:category, category_id: category_1.id) }
 
